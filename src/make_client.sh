a0d0d142c27522bcb1490c6e6cd8e2db48cdb90e
Anoj Perera <anojmperera@gmail.com>
2015-04-15 22:20:16 +0100

    Make scripts updated.

--- src/make_client.sh
+++ src/make_client.sh
@@ -6,7 +6,6 @@ grand_parent=$(dirname $parent )
 thor_include_folder="thor/inc/"
 thor_lib_folder="thor/bin/"
 
-
 include_folder="/include/"
 include_path="$parent$include_folder"
 thor_inc_path="$grand_parent/$thor_include_folder"
@@ -14,9 +13,9 @@ thor_lib_path="$grand_parent/$thor_lib_folder"
 jsmn_inc_path="$parent/external/jsmn/"
 
 
-gcc -g -Wall -O0 -o ../bin/client uclient.c utilsint.c jsonint.c $jsmn_inc_path/jsmn.c \
+gcc -g -Wall -O0 -o ../bin/client uclient.c utilsint.c jsonint.c unzbget.c nzbgetint.c $jsmn_inc_path/jsmn.c \
 	-I$include_path -I/usr/include/libxml2/ -I$thor_inc_path -I$jsmn_inc_path \
 	-L$thor_lib_path -Wl,-rpath=$thor_lib_path \
-	-lcomm -lalist -lm -lconfig -lcurl -lxml2 -lssl -lcrypto -lpthread
+	-lcomm -lalist -lm -lconfig -lxmlrpc_util -lxmlrpc_client -lxmlrpc -lcurl -lxml2 -lssl -lcrypto -lpthread
 
 exit 0
--- src/make_server.sh
+++ src/make_server.sh
@@ -10,13 +10,15 @@ glist_lib_folder="g_list/bin/libalist.a"
 include_folder="/include/"
 include_path="$parent$include_folder"
 thor_inc_path="$grand_parent/$thor_include_folder"
+jsmn_inc_path="$parent/external/jsmn/"
+
 thor_lib_path="$grand_parent/$thor_lib_folder"
 glist_lib_path="$grand_parent/$glist_lib_folder"
 
 # Make server
 gcc -g -Wall -O0 -o ../bin/server userver.c utilsint.c \
 	 $thor_lib_path $glist_lib_path \
-	-I$include_path -I/usr/include/libxml2/ -I$thor_inc_path \
+	-I$include_path -I$jsmn_inc_path -I/usr/include/libxml2/ -I$thor_inc_path \
 	-lm -lpthread -lconfig -lcurl -lxml2 -lssl -lcrypto
 
 exit 0
